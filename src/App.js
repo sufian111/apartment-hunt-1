88cc7fc447d0651b48c68121f945050f26c1e410
Tamim <tamim-hossain>
2020-11-16 00:23:53 +0600

    home page done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./components/Home/Home/Home";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
