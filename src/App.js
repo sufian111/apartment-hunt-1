f63e2fbe67737fd58b39420d857280aea615b874
Tamim <tamim-hossain>
2020-11-15 23:51:06 +0600

    working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import Facebook from './components/LogIn/Facebook';
-import Google from './components/LogIn/Google';
-import SignUp from './components/LogIn/SignUp';
 
 const App = () => {
   return (
     <div>
-      <Facebook/>
-      <Google/><br/><br/>
-      <SignUp/>
+      
     </div>
   );
 };
