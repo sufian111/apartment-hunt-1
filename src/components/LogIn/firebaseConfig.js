234ce035ce0a28a45a4d0d1566df330f6c92ec9d
Tamim <tamim-hossain>
2020-11-15 12:39:33 +0600

    working

---
+++ src/components/LogIn/firebaseConfig.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: 'AIzaSyArjKL55dTXOBoUDAurSDTShJDRhGkhdMM',
+    authDomain: 'hunt-apartment.firebaseapp.com',
+    databaseURL: 'https://hunt-apartment.firebaseio.com',
+    projectId: 'hunt-apartment',
+    storageBucket: 'hunt-apartment.appspot.com',
+    messagingSenderId: '923315586453',
+    appId: '1:923315586453:web:323271b0d93fa43acf629c',
+    measurementId: 'G-MZP2NRP2WW',
+};
+
+export default firebaseConfig;
