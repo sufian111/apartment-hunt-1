e055ddb8506d824a4a81b0341331f534cbd8848d
Rayhan0Islam0Shagor <67514865+Rayhan0Islam0Shagor@users.noreply.github.com>
2020-11-16 00:19:18 +0600

    footer arektu baki

--- src/components/Home/Home/Footer/Footer.js
+++ src/components/Home/Home/Footer/Footer.js
@@ -2,8 +2,24 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>This is Footer</h1>
+        <div style={{ backgroundColor: "#275A53" }}>
+            <div className="row">
+                <div className="col-md-4">
+                    <p>H#340 (4th Floor), Road #24,</p>
+                    <p>New DOHS, Mohakhali, Dhaka, Bangladesh</p>
+                    <p>Phone: 018XXXXXXXX</p>
+                    <p>E-mail: info@company.com</p>
+                </div>
+                <div className="col-md-2">
+
+                </div>
+                <div className="col-md-2">
+
+                </div>
+                <div className="col-md-4">
+
+                </div>
+            </div>
         </div>
     );
 };
