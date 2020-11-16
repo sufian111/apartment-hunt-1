c3ad1abdf42503b5860925dbb6ae23616e577ee6
Tamim <tamim-hossain>
2020-11-16 10:58:01 +0600

    ui done

--- src/App.js
+++ src/App.js
@@ -1,62 +1,56 @@
-import { createContext, useState } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import ApartmentDetail from "./components/Home/ApartmentDetail/ApartmentDetails";
-import AddHouse from "./components/Home/Dashboard/AddHouse/AddHouse";
-import BookingList from "./components/Home/Dashboard/BookingList/BookingList";
-import MyRent from "./components/Home/Dashboard/MyRent/MyRent";
-import Home from "./components/Home/Home/Home";
-import Login from "./components/Home/Login/Login";
+import { createContext, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ApartmentDetail from './components/Home/ApartmentDetail/ApartmentDetails';
+import AddHouse from './components/Home/Dashboard/AddHouse/AddHouse';
+import BookingList from './components/Home/Dashboard/BookingList/BookingList';
+import MyRent from './components/Home/Dashboard/MyRent/MyRent';
+import Home from './components/Home/Home/Home';
+import Login from './components/Home/Login/Login';
 
 export const UserContext = createContext();
 
 const App = () => {
-  const [user, setUser] = useState({});
+    const [user, setUser] = useState({});
 
-  return (
-    <UserContext.Provider value={[user, setUser]}>
-      <Router>
-        <Switch>
+    return (
+        <UserContext.Provider value={[user, setUser]}>
+            <Router>
+                <Switch>
+                    <Route path='/home'>
+                        <Home />
+                    </Route>
 
-          <Route path="/home">
-            <Home />
-          </Route>
+                    <Route path='/login'>
+                        <Login />
+                    </Route>
 
-          <Route path="/login">
-            <Login />
-          </Route>
+                    <Route path='/services'>
+                        <ApartmentDetail />
+                    </Route>
 
-          <Route path="/services">
-            <ApartmentDetail />
-          </Route>
+                    <Route path='/dashboard'>
+                        <BookingList />
+                    </Route>
 
-          <Route path="/dashboard">
-            <BookingList />
-          </Route>
+                    <Route path='/bookingList'>
+                        <BookingList />
+                    </Route>
 
-          <Route path="/bookingList">
-            <BookingList />
-          </Route>
+                    <Route path='/addHouse'>
+                        <AddHouse />
+                    </Route>
 
-          <Route path="/addHouse">
-            <AddHouse />
-          </Route>
+                    <Route path='/myRent'>
+                        <MyRent />
+                    </Route>
 
-          <Route path="/myRent">
-            <MyRent />
-          </Route>
-
-          <Route path="/">
-            <Home />
-          </Route>
-
-        </Switch>
-      </Router>
-    </UserContext.Provider>
-  );
+                    <Route path='/'>
+                        <Home />
+                    </Route>
+                </Switch>
+            </Router>
+        </UserContext.Provider>
+    );
 };
 
 export default App;
