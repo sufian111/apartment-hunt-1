24667b28d58c22a1c8d9e5ef54c48badecb531e5
Tamim <tamim-hossain>
2020-11-17 21:28:41 +0600

    working

--- src/components/Home/ApartmentDetail/ApartmentDescripton/ApartmentDescription.js
+++ src/components/Home/ApartmentDetail/ApartmentDescripton/ApartmentDescription.js
@@ -12,69 +12,92 @@ const ApartmentDescription = () => {
 
     const loadData = async () => {
         await fetch('http://localhost:4000/bookings/' + id)
-            .then(res => res.json())
-            .then(data => setHotel(data))
-    }
+            .then((res) => res.json())
+            .then((data) => setHotel(data));
+    };
 
     const { name, price } = hotel;
 
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-8 col-sm-12">
-                    <div className="mt-4">
-                        <div className="d-flex font-weight-bold justify-content-between">
-                            <h1 className="font-weight-bold" style={{ color: "#16322E" }}>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-8 col-sm-12'>
+                    <div className='mt-4'>
+                        <div className='d-flex font-weight-bold justify-content-between'>
+                            <h1
+                                className='font-weight-bold'
+                                style={{ color: '#16322E' }}
+                            >
                                 {name}
                             </h1>
-                            <h1 className="font-weight-bold" style={{ color: "#16322E" }}>
+                            <h1
+                                className='font-weight-bold'
+                                style={{ color: '#16322E' }}
+                            >
                                 ${price}
                             </h1>
                         </div>
                         <p>
-                            3000 sq-ft., 3 Bedroom, Semi-furnished, Luxurious, South facing
-                            Apartment for Rent in Rangs Malancha, Melbourne.
+                            3000 sq-ft., 3 Bedroom, Semi-furnished, Luxurious,
+                            South facing Apartment for Rent in Rangs Malancha,
+                            Melbourne.
                         </p>
                     </div>
-                    <div className="mt-5">
-                        <h3 className="font-weight-bold" style={{ color: "#16322E" }}>
+                    <div className='mt-5'>
+                        <h3
+                            className='font-weight-bold'
+                            style={{ color: '#16322E' }}
+                        >
                             Price Details-
                         </h3>
-                        <ul className="list-unstyled">
+                        <ul className='list-unstyled'>
                             <li>Rent/Month: $550 (negotiable)</li>
-                            <li>Service Charge : 8,000/= Tk per month, subject to change</li>
+                            <li>
+                                Service Charge : 8,000/= Tk per month, subject
+                                to change
+                            </li>
                             <li>Security Deposit : 3 month’s rent</li>
-                            <li>Flat Release Policy : 3 months earlier notice required</li>
+                            <li>
+                                Flat Release Policy : 3 months earlier notice
+                                required
+                            </li>
                         </ul>
                     </div>
-                    <div className="mt-5 mb-5">
-                        <h3 className="font-weight-bold" style={{ color: "#16322E" }}>
+                    <div className='mt-5 mb-5'>
+                        <h3
+                            className='font-weight-bold'
+                            style={{ color: '#16322E' }}
+                        >
                             Property Details-
                         </h3>
-                        <ul className="list-unstyled">
+                        <ul className='list-unstyled'>
                             <li>
-                                Address & Area : Rangs Malancha, House-68, Road-6A (Dead End
-                                Road), Dhanmondi Residential Area.
+                                Address & Area : Rangs Malancha, House-68,
+                                Road-6A (Dead End Road), Dhanmondi Residential
+                                Area.
                             </li>
                             <li>Flat Size : 3000 Sq Feet.</li>
                             <li>
-                                Floor : A5 (5th Floor) (6 storied Building ) (South Facing Unit)
+                                Floor : A5 (5th Floor) (6 storied Building )
+                                (South Facing Unit)
                             </li>
                             <li>
-                                Room Category : 3 Large Bed Rooms with 3 Verandas, Spacious
-                                Drawing, Dining & Family Living Room, Highly Decorated Kitchen
-                                with Store Room and Servant room with attached
+                                Room Category : 3 Large Bed Rooms with 3
+                                Verandas, Spacious Drawing, Dining & Family
+                                Living Room, Highly Decorated Kitchen with Store
+                                Room and Servant room with attached
                             </li>
                             <li>
-                                Facilities : 1 Modern Lift, All Modern Amenities & Semi
-                                Furnished.
+                                Facilities : 1 Modern Lift, All Modern Amenities
+                                & Semi Furnished.
                             </li>
                             <li>
-                                Additional Facilities : a. Electricity with full generator load,
-                                b. Central Gas Geyser, c. 2 Car Parking with 1 Driver’s
-                                Accommodation, d. Community Conference Hall, e. Roof Top
-                                Beautified Garden and Grassy Ground, f. Cloth Hanging facility
-                                with CC camera
+                                Additional Facilities : a. Electricity with full
+                                generator load, b. Central Gas Geyser, c. 2 Car
+                                Parking with 1 Driver’s Accommodation, d.
+                                Community Conference Hall, e. Roof Top
+                                Beautified Garden and Grassy Ground, f. Cloth
+                                Hanging facility with CC camera
                             </li>
                         </ul>
                     </div>
